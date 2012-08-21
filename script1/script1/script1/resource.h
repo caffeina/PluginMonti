0c2f980b93e799160d1fd5ce3a2c1ec3afac6035
caffeina <aniello.iarnone@gmail.com>
2012-08-21 13:56:38 +0200

    questo file github non l'ha aggiunto
    mannaggia

---
+++ script1/script1/script1/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by script1.rc
+//
+#define IDD_DIALOG1                     7000
+#define IDC_COMBO1                      7000
+#define IDC_EDIT1                       7001
+#define IDC_COMBO2                      7002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        7001
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         7003
+#define _APS_NEXT_SYMED_VALUE           7000
+#endif
+#endif
