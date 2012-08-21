e999b8ee726d95281e154d20154dff4f837ffe44
caffeina <aniello.iarnone@gmail.com>
2012-08-21 16:29:33 +0200

    aggiunta del file resource...
    github non l'ha aggiunto.... mah

---
+++ script1/script1/script1/Resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by script1.rc
+//
+#define IDD_DIALOG1                     7000
+#define IDC_COMBO1                      7005
+#define IDC_EDIT1                       7001
+#define IDC_COMBO2                      7002
+#define IDC_CHECK1                      7003
+#define IDC_SCROLLBAR1                  7009
+#define IDC_COMBO3                      7010
+#define IDC_EDIT2                       7011
+#define IDC_COMBO4                      7012
+#define IDC_COMBO5                      7013
+#define IDC_BUTTON1                     7014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        7002
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         7015
+#define _APS_NEXT_SYMED_VALUE           7000
+#endif
+#endif
