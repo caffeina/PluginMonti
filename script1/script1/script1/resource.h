22df501d856554a43af46fc802ccd365b07bd117
Aniello <aniello.iarnone@gmail.com>
2012-08-25 17:31:32 +0200

    ha saltato il sync di questo file
    relativo all'ultimo commit

---
+++ script1/script1/script1/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by script1.rc
+//
+#define IDD_DIALOG1                     7000
+#define IDC_EDIT1                       7001
+#define IDC_COMBO2                      7002
+#define IDC_CHECK1                      7003
+#define IDP_OLE_INIT_FAILED             7003
+#define IDC_COMBO1                      7005
+#define IDC_SCROLLBAR1                  7009
+#define IDC_COMBO3                      7010
+#define IDC_EDIT2                       7011
+#define IDC_COMBO4                      7012
+#define IDC_COMBO5                      7013
+#define IDC_BUTTON1                     7014
+#define IDC_EDIT3                       7015
+#define IDC_EDIT4                       7016
+#define IDC_EDIT5                       7017
+#define IDC_EDIT6                       7018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        7003
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         7019
+#define _APS_NEXT_SYMED_VALUE           7000
+#endif
+#endif
